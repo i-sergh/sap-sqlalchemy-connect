9aaac7f2baef1509b91ae71f1c6547e1da4c8b4d
sergh <seripp28@gmail.com>
2023-08-17 10:11:52 +0300

    fix hana query to declarative style

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from sqlalchemy import text, select, Insert
 
 
 with SessionHana() as session:    
-    sql = select(HanaMARA)
-    result =session.execute(text(str(sql)))
+    sql = select(HanaMARA.__table__)
+    result =session.execute(sql)
     result_list = result.all()
-
+    
 ## sql query be like : ..brrrr
 
 #text('SELECT "ABAPS417"."MARA"."MANDT", "ABAPS417"."MARA"."MATNR", "ABAPS417"."MARA"."ERSDA", "ABAPS417"."MARA"."ERNAM", "ABAPS417"."MARA"."LAEDA", "ABAPS417"."MARA"."AENAM", "ABAPS417"."MARA"."VPSTA", "ABAPS417"."MARA"."PSTAT", "ABAPS417"."MARA"."LVORM", "ABAPS417"."MARA"."MTART", "ABAPS417"."MARA"."MBRSH", "ABAPS417"."MARA"."MATKL", "ABAPS417"."MARA"."BISMT", "ABAPS417"."MARA"."MEINS", "ABAPS417"."MARA"."BSTME", "ABAPS417"."MARA"."ZEINR", "ABAPS417"."MARA"."ZEIAR" \
